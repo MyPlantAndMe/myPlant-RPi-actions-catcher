20bd110c762f660b1f9345e9665475478398b995
dgellow <samuel.elborai@gmail.com>
2015-04-19 12:47:23 +0200

    add logs

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('localhost', 3100))
 
 def write(filename, data):
+    print 'Write data ' + str(data) + ' to file ' + str(filename)
     with open(filename, 'w') as f:
         f.write(data)
 
@@ -14,13 +15,15 @@ def readJson(bytes):
     return json.loads(bytes.decode('utf8'))
 
 def receiveJson():
+    print 'Read from socket'
     return readJson(sock.recv(4096))
 
 def dispatch(sensor, duration):
+    print 'Dispatch data "' + str(duration) + '" to sensor ' + str(sensor)
     if (sensor in ['lights', 'fan', 'water']):
         write(sensor, duration)
     else:
-        print 'Invalid sensor name: ' + sensor
+        print 'Invalid sensor name: ' + str(sensor)
 
 def main():
     response = receiveJson()

bb8f9770ab4bcb245f6d427e91aafbaa812a8419
dgellow <samuel.elborai@gmail.com>
2015-04-19 08:42:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Catch actions from myplantandme-server's TCP server and write (not append) a duration (in seconds) into a file.
+
+Uses Python 2.7.
+
+# Run
+
+```
+# myplantandme-server should be running
+python2.7 client.py
+```
+
+# Results
+- file `lights`
+- file `fan`
+- file `water`
+
+Exemple of content:
+`1800`
---
+++ client.py
@@ -0,0 +1,32 @@
+#!/bin/env python2.7
+import socket
+import json
+import time
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('localhost', 3100))
+
+def write(filename, data):
+    with open(filename, 'w') as f:
+        f.write(data)
+
+def readJson(bytes):
+    return json.loads(bytes.decode('utf8'))
+
+def receiveJson():
+    return readJson(sock.recv(4096))
+
+def dispatch(sensor, duration):
+    if (sensor in ['lights', 'fan', 'water']):
+        write(sensor, duration)
+    else:
+        print 'Invalid sensor data: ' + sensor
+
+def main():
+    response = receiveJson()
+    duration = response.get('duration')
+    sensor = response.get('sensor')
+    dispatch(sensor, duration)
+
+while True:
+    main()

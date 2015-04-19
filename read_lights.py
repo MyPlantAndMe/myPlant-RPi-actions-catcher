131981bfec7e42eb1643cc949f4aa428718a9c75
dgellow <samuel.elborai@gmail.com>
2015-04-19 13:39:40 +0200

    use pi-blaster to manage gpio

--- read_lights.py
+++ read_lights.py
@@ -3,15 +3,16 @@ from subprocess import call
 import time
 
 filename = 'lights'
-call(['gpio', 'mode', '27', 'out'])
 
 def enable():
     print 'enable'
-    call(['gpio', 'write', '27', '1'])
+    with open('/dev/pi-blaster', 'w') as outfile:
+        call(['echo', '"25=1"'], stdout=outfile)
 
 def disable():
     print 'disable'
-    call(['gpio', 'write', '27', '0'])
+    with open('/dev/pi-blaster', 'w') as outfile:
+        call(['echo', '"25=0"'], stdout=outfile)
 
 def wait(seconds):
     print 'wait ' + str(seconds) + ' seconds'
